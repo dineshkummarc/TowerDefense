00553fb2ba9b23c48de80dcacb0681960b2e7781
jachymko <jachymko@hotmail.com>
2010-02-03 19:06:50 +0100

    added missing files

---
+++ js/TowerDefense/Map.js
@@ -0,0 +1,83 @@
+﻿(function($, undefined)
+{
+    window.TowerDefense.Map = function(game, width, height)
+    {
+        var map = this;
+        var places = [];
+        
+        places.length = (width * height);
+        
+        map.width = width;
+        map.height = height;
+        map.objects = [];
+        
+        function get(x, y)
+        {   
+            return places[y*width + x];
+        }
+        
+        function set(x, y, value)
+        {
+            places[y*width + x] = value;
+        }
+        
+        function enumArea(left, top, width, height, callback)
+        {
+            for (var y = top; y < (top + height); y++)
+            {
+                for (var x = left; x < (left + width); x++)
+                {
+                    var value = get(x, y);
+                    
+                    if (callback(value, x, y) === false)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+        
+        map.at = function(x, y)
+        {
+            return get(x, y);
+        };
+        
+        map.isEmpty = function(left, top, width, height)
+        {
+            var result = true;
+            
+            enumArea(left, top, width, height, function(content)
+            {
+                if (content)
+                {
+                    result = false;
+                    return false;
+                }
+            });
+            
+            return result;
+        };
+        
+        map.addObject = function(obj)
+        {
+            var left = obj.x();
+            var top = obj.y();
+            
+            var size = obj.size();
+            
+            if (map.isEmpty(left, top, size, size))
+            {
+                enumArea(left, top, size, size, function(content, x, y)
+                {
+                    set(x, y, obj);
+                });
+                
+                map.objects.push(obj);
+                return true;
+            }
+            
+            return false;
+        };
+    };
+    
+})(jQuery);
---
+++ js/TowerDefense/MapObject.js
@@ -0,0 +1,155 @@
+﻿(function($, undefined)
+{
+    window.TowerDefense.MapObject = function(game, size, range)
+    {
+        /* @const */ var PI     = Math.PI;
+        /* @const */ var TwoPI  =    2*PI;
+
+        var self = this;
+        var x, y;
+        
+        function xscaled()
+        {
+            return game.scale(self.x());
+        }
+
+        function yscaled()
+        {
+            return game.scale(self.y());
+        }
+
+        self.isPlaceholder = false;
+        self.isPlaceholderValid = false;
+        
+        self.isSelected = false;
+
+        self.isInRange = function(tx, ty)
+        {
+            if (range > 0)
+            {
+                var absdx = Math.abs(tx - self.x());
+                var absdy = Math.abs(ty - self.y());
+
+                return (Math.sqrt(absdx*absdx + absdy*absdy) <= range);
+            }
+            
+            return false;
+        };
+
+        self.size = function()
+        {
+            return size;
+        }
+
+        self.x = function(newx)
+        {
+            if (newx !== undefined)
+            {
+                x = newx;
+            }
+            
+            return x;
+        };
+        
+        self.y = function(newy)
+        {
+            if (newy !== undefined)
+            {
+                y = newy;
+            }
+            
+            return y;
+        };
+
+        self.paintPlaceholder = function(context)
+        {
+            var sz = game.scale(size);
+            
+            context.save();
+            
+            context.fillStyle = self.isPlaceholderValid ? 'rgba(0, 255, 0, 0.5)' : 'rgba(255, 0, 0, 0.5)';
+            context.fillRect(0, 0, sz, sz);
+
+            context.strokeStyle = self.isPlaceholderValid ? 'green' : 'red';
+            context.strokeRect(0, 0, sz, sz);                             
+            
+            context.restore();
+        };
+
+        self.paintRange = function(context)
+        {
+            context.beginPath();
+            
+            context.arc(game.scale(size/2),
+                        game.scale(size/2), 
+                        game.scale(range), 
+                        0, TwoPI);
+            
+            context.closePath();
+            
+            context.lineWidth = 1;
+            context.strokeStyle = self.isPlaceholderValid ? 'green' : 'red';
+            context.stroke();
+        };
+        
+        self.paintSelection = function(context)
+        {
+            var sz = game.scale(size);        
+            var z = game.scale(0.4);
+
+            context.beginPath();
+
+            context.moveTo(0, z);
+            context.lineTo(0, 0);
+            context.lineTo(z, 0);
+
+            context.moveTo(sz - z, 0);
+            context.lineTo(sz,     0);
+            context.lineTo(sz,     z);
+
+            context.moveTo(sz,     sz - z);
+            context.lineTo(sz,     sz);
+            context.lineTo(sz - z, sz);
+
+            context.moveTo(z, sz);
+            context.lineTo(0, sz);
+            context.lineTo(0, sz - z);
+            
+            context.lineWidth = 1;
+            context.strokeStyle = 'white';
+            context.stroke();
+        };
+        
+        self.paintObject = function(context)
+        {
+        };
+        
+        self.paint = function(context)
+        {
+            context.save();
+            context.translate(xscaled(), yscaled());
+                    
+            if (self.isPlaceholder)
+            {
+                self.paintPlaceholder(context);
+            }
+            else
+            {
+                self.paintObject(context);
+            }
+            
+            if (self.isPlaceholder || self.isSelected)
+            {
+                self.paintRange(context);
+            }
+            
+            if (self.isSelected)
+            {
+                self.paintSelection(context);
+            }
+            
+            context.restore();
+        };
+    };
+    
+})(jQuery);
